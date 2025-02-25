32a752aa840c7376149f19fccba5b636e1bf067b
Aleksei Sudarev <aleksei.sudarev96@gmail.com>
2025-02-25 18:07:06 +0300

    Add db context interface for cinema

---
+++ Cinema.Application/Interfaces/ICinemaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Application.Interfaces
+{
+    public interface ICinemaDbContext
+    {
+        DbSet<Movie> Movies { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}

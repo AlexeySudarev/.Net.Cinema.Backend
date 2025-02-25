e829dfa7dd45c5fdf52b4468acd8aa33d045cb16
Aleksei Sudarev <aleksei.sudarev96@gmail.com>
2025-02-25 20:11:12 +0300

    Add cinema db configuration

---
+++ Cinema.Database/CinemaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Cinema.Application.Interfaces;
+using Cinema.Database.EntityConfigurations;
+using Cinema.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Database
+{
+    class CinemaDbContext : DbContext, ICinemaDbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CinemaConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Cinema.Database/EntityConfigurations/CinemaConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Cinema.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.Database.EntityConfigurations
+{
+    class CinemaConfiguration : IEntityTypeConfiguration<Movie>
+    {
+        private const int StringMaxLength = 100;
+
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasKey(movie => movie.Id);
+            builder.Property(movie => movie.Title).IsRequired().HasMaxLength(StringMaxLength);
+            builder.Property(movie => movie.Year).IsRequired();
+            builder.Property(movie => movie.AgeRating).IsRequired();
+            builder.Property(movie => movie.Country).IsRequired();
+            builder.Property(movie => movie.Type).IsRequired();
+
+            builder.HasIndex(movie => movie.Title).IsUnique();
+        }
+    }
+}

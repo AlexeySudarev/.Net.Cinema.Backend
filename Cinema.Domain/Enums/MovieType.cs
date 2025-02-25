2659feb3c556537ce49d173e53aaf7cd5b6df247
Aleksei Sudarev <aleksei.sudarev96@gmail.com>
2025-02-25 17:28:15 +0300

    Add movie entity

---
+++ Cinema.Domain/Enums/MovieType.cs
@@ -0,0 +1,23 @@
+﻿namespace Cinema.Domain.Enums
+{
+    [Flags]
+    public enum MovieType
+    {
+        None = 0,
+        Biography = 1 << 0,
+        Drama = 1 << 1,
+        Melodrama = 1 << 2,
+        Musical = 1 << 3,
+        Detective = 1 << 4,
+        Action = 1 << 5,
+        History = 1 << 6,
+        Comedy = 1 << 7,
+        Cartoon = 1 << 8,
+        Fantasy = 1 << 9,
+        Family = 1 << 10,
+        Animation = 1 << 11,
+        Adventure = 1 << 12,
+        SciFi = 1 << 13,
+        War = 1 << 14
+    }
+}
---
+++ Cinema.Domain/Movie.cs
@@ -0,0 +1,14 @@
+﻿using Cinema.Domain.Enums;
+
+namespace Cinema.Domain
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; }
+        public int AgeRating { get; set; }
+        public CountryType Country { get; set; }
+        public MovieType Type { get; set; }
+    }
+}

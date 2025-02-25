8aa8969650887f4ec49a61fa59bfca39bcb355e5
Aleksei Sudarev <aleksei.sudarev96@gmail.com>
2025-02-25 20:01:44 +0300

    Remove annotation attributes from movie entity

--- Cinema.Domain/Movie.cs
+++ Cinema.Domain/Movie.cs
@@ -1,26 +1,14 @@
 ﻿using Cinema.Domain.Enums;
-using System.ComponentModel.DataAnnotations;
 
 namespace Cinema.Domain
 {
     public class Movie
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
         public string Title { get; set; }
-
-        [Required]
         public int Year { get; }
-
-        [Required]
         public int AgeRating { get; set; }
-
-        [Required]
         public CountryType Country { get; set; }
-
-        [Required]
         public MovieType Type { get; set; }
     }
 }
